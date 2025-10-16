c5b0d0bf395bdb6d4c993a8915c1ad52a0531b80
Vasiluk Dmitry <vasilukwolf@gmail.com>
2025-10-16 15:58:56 +0400

    Entity (#4)
    * PropertyFilter: Sparse Fieldsets
    
    * Hal&CSV
    
    * Validation
    
    * Creating a User Entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\DragonTreasureFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,6 +11,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-         DragonTreasureFactory::createMany(40);
+        DragonTreasureFactory::createMany(40);
+        UserFactory::createMany(10);
     }
 }
