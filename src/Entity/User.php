d6ab9d129d609e895f984262467918b5be3becb8
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2025-10-17 16:01:58 +0400

    Removing Items from a Collection

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -52,7 +52,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[Assert\NotBlank]
     private ?string $username = null;
 
-    #[ORM\OneToMany(mappedBy: 'owner', targetEntity: DragonTreasure::class, cascade: ['persist'])]
+    #[ORM\OneToMany(mappedBy: 'owner', targetEntity: DragonTreasure::class, cascade: ['persist'], orphanRemoval: true)]
     #[Groups(['user:read', 'user:write'])]
     #[Assert\Valid]
     private Collection $dragonTreasures;
